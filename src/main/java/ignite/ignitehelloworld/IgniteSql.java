db207fc0c16e3a84e86422c776869aa213131e9c
andresr31 <carlos.andres.1031@gmail.com>
2020-05-17 00:32:27 -0500

    Add SqlExample

--- src/main/java/ignite/ignitehelloworld/IgniteSql.java
+++ src/main/java/ignite/ignitehelloworld/IgniteSql.java
@@ -6,26 +6,25 @@
 package ignite.ignitehelloworld;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteException;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.query.SqlFieldsQuery;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.lang.IgniteRunnable;
 import org.apache.ignite.resources.IgniteInstanceResource;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.multicast.TcpDiscoveryMulticastIpFinder;
-import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 
 /**
  *
- * @author Carlos Andrés Rojas Parra 
- * @author David Salgado Ospina
+ * @author Otro
  */
-public class IgniteHelloWorld {
+public class IgniteSql {
+    
     public static void main(String[] args) throws IgniteException {
         // Preparing IgniteConfiguration using Java APIs
         IgniteConfiguration cfg = new IgniteConfiguration();
@@ -35,55 +34,73 @@ public class IgniteHelloWorld {
 
         // Classes of custom Java logic will be transferred over the wire from this app.
         cfg.setPeerClassLoadingEnabled(true);
-
+        
         // Node`s Ips
         Collection<String> ips = new ArrayList<>();
         ips.add("127.0.0.1");
         //ips.add("192.168.1.75");
-        
+
         // Setting up an IP Finder to ensure the client can locate the servers.
         TcpDiscoveryMulticastIpFinder ipFinder = new TcpDiscoveryMulticastIpFinder();
         ipFinder.setAddresses(ips);
         cfg.setDiscoverySpi(new TcpDiscoverySpi().setIpFinder(ipFinder));
 
-        // Start Ignite node.
+        // Starting the node
         Ignite ignite = Ignition.start(cfg);
 
         // Create an IgniteCache and put some values in it.
         IgniteCache<Integer, String> cache = ignite.getOrCreateCache("myCache");
-        cache.put(1, "Hello");
-        cache.put(2, "World!");
+        // Create table based on PARTITIONED template with one backup.
+        cache.query(new SqlFieldsQuery(
+                "CREATE TABLE persona ( id INT PRIMARY KEY, nombre VARCHAR(20));")).getAll();
+        
+        SqlFieldsQuery qry = new SqlFieldsQuery(
+            "insert into persona (id, nombre) values (?, ?)");
 
-        System.out.println(">> Created the cache and add the values.");
+        cache.query(qry.setArgs(1,"Andres"));
+        cache.query(qry.setArgs(2,"David"));
+        
+        System.out.println(">> Created the cache and values.");
 
         // Executing custom Java compute task on server nodes.
-        ignite.compute(ignite.cluster().forServers()).broadcast(new RemoteTask());
+        ignite.compute(ignite.cluster().forServers()).broadcast((IgniteRunnable) new RemoteTask());
 
         System.out.println(">> Compute task is executed, check for output on the server nodes.");
+
+        System.out.println(">> Obteniendo valores nuevamente");
+        System.out.println(cache.query(new SqlFieldsQuery(
+         "SELECT * FROM persona;")).getAll());
+
 
         // Disconnect from the cluster.
         ignite.close();
     }
 
     /**
-     * A compute tasks that prints out a node ID and some details about its OS and JRE.
-     * Plus, the code shows how to access data stored in a cache from the compute task.
+     * A compute tasks that prints out a node ID and some details about its OS
+     * and JRE. Plus, the code shows how to access data stored in a cache from
+     * the compute task.
      */
     private static class RemoteTask implements IgniteRunnable {
+
         @IgniteInstanceResource
         Ignite ignite;
 
-        @Override public void run() {
+        @Override
+        public void run() {
             System.out.println(">> Executing the compute task");
 
             System.out.println(
-                "   Node ID: " + ignite.cluster().localNode().id() + "\n" +
-                "   OS: " + System.getProperty("os.name") +
-                "   JRE: " + System.getProperty("java.runtime.name"));
+                    "   Node ID: " + ignite.cluster().localNode().id() + "\n"
+                    + "   OS: " + System.getProperty("os.name")
+                    + "   JRE: " + System.getProperty("java.runtime.name"));
 
             IgniteCache<Integer, String> cache = ignite.cache("myCache");
 
-            System.out.println(">> " + cache.get(1) + " " + cache.get(2));
+            System.out.println(">> Hello world sql");
         }
     }
+
+    
+    
 }
